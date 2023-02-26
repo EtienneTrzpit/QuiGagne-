37b053f51a6593c682f3c7c134504c9a71312192
EtienneTrzpit <etienne.trzpit@free.fr>
2023-02-26 17:48:50 +0100

    correction 1 premières images

--- jeux.js
+++ jeux.js
@@ -2,7 +2,7 @@ let soldat_romain = document.getElementById("romain");
 let soldat_fusil = document.getElementById("fusil");
 let canvas = document.getElementById("c1");
 let ctx = canvas.getContext("2d");
-soldat_fusil.addEventListener('load', function(){
+ctx.addEventListener('load', function(){
     ctx.drawImage(soldat_romain, 75, 50, 50, 75);
     ctx.drawImage(soldat_fusil, 250, 50, 50, 75);
 })
