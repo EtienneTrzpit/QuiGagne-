ab2624e51f0ceec7e5a93b9fa2e62431b7d9af60
EtienneTrzpit <etienne.trzpit@free.fr>
2023-03-01 18:05:30 +0100

    affichage des deux images 5

--- jeux.js
+++ jeux.js
@@ -2,7 +2,7 @@
     let soldat_fusil = document.getElementById("fusil");
     let canvas = document.getElementById("c1");
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(soldat_romain, 75, 50, 50, 75);
-    ctx.drawImage(soldat_fusil, 150, 50, 50, 75);
+    ctx.drawImage(soldat_romain, 50, 50, 50, 75);
+    ctx.drawImage(soldat_fusil, 125, 50, 50, 75);
 
 
