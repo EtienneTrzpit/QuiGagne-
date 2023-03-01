856a3222170f5d77c26bbf642ffc9d75767e2edc
EtienneTrzpit <etienne.trzpit@free.fr>
2023-03-01 16:40:42 +0100

    affichage des deux images

--- jeux.js
+++ jeux.js
@@ -1,10 +1,8 @@
-function pb(){
     let soldat_romain = document.getElementById("romain");
     let soldat_fusil = document.getElementById("fusil");
     let canvas = document.getElementById("c1");
     let ctx = canvas.getContext("2d");
     ctx.drawImage(soldat_romain, 75, 50, 50, 75);
-}
-setTimeout(pb, 10000);
+    ctx.drawImage(soldat_fusil, 275, 50, 50, 75);
 
 
