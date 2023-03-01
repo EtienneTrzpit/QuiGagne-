10f8917dbdb85312e5922050c886354b33248260
EtienneTrzpit <etienne.trzpit@free.fr>
2023-03-01 11:25:08 +0100

    correction 7 premières images

--- jeux.js
+++ jeux.js
@@ -1,9 +1,10 @@
 function pb(){
     let canvas = document.getElementById("c1");
     let ctx = canvas.getContext("2d");
+    ctx.drawImage(soldat_romain, 75, 50, 50, 75);
 }
 let soldat_romain = document.getElementById("romain");
 let soldat_fusil = document.getElementById("fusil");
 setTimeout(pb, 10000);
-ctx.drawImage(soldat_romain, 75, 50, 50, 75);
+
 
