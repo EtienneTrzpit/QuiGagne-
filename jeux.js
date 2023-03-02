8022253a5bbf5b6d0645f5e87bf23d7811c5e40a
EtienneTrzpit <etienne.trzpit@free.fr>
2023-03-02 15:06:50 +0100

    affichage des flèches 2

--- jeux.js
+++ jeux.js
@@ -4,8 +4,8 @@
     let fleche_droite = document.getElementById("droite");
     let canvas = document.getElementById("c1");
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(soldat_romain, 50, 50, 50, 75);
-    ctx.drawImage(soldat_fusil, 125, 50, 50, 75);
-    ctx.drawImage(gauche, 0, 50, 50, 75);
-    ctx.drawImage(droite, 200, 50, 50, 75);
+    ctx.drawImage(soldat_romain, 30, 30, 25, 50);
+    ctx.drawImage(soldat_fusil, 90, 30, 25, 50);
+    ctx.drawImage(fleche_droite, 0, 30, 25, 50);
+    ctx.drawImage(fleche_gauche, 150, 30, 25, 50);
 
